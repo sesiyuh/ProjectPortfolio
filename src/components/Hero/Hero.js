db6759515645c91e8d1ee34b1a7effe2eea00cda
sesiyuh <mpmariano@tip.edu.ph>
2025-05-20 23:53:20 +0800

    Updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,18 +1,20 @@
-// Home.jsx
 import React from "react";
 import "./Hero.css";
 import Social from "./Social";
 import Data from "./Data";
 import ScrollDown from "./ScrollDown";
-import profileImg from "../../assets/me.png"; // Import the image
+// Import the profile image directly
+import profileImg from "../../assets/me.png";
 
-const Home = () => {
+const Hero = () => {
   return (
     <section className="home section" id="home">
       <div className="home__container container grid">
         <div className="home__content grid">
           <Social />
-          <div className="home__img" style={{ backgroundImage: `url(${profileImg})` }}>
+          {/* Create an actual img element instead of a background image */}
+          <div className="home__img-container">
+            <img src={profileImg} alt="Profile" className="home__img" />
           </div>
           <Data />
         </div>
@@ -22,4 +24,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Hero;
